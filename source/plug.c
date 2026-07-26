9e5a42f53c14e2a7062ec1c7e216f1704eb67fdc
tiredsapines <endrit.buzi@gmail.com>
2026-07-26 16:36:23 +0200

    bugfixes and changes

--- source/plug.c
+++ source/plug.c
@@ -22,10 +22,10 @@ typedef struct {
     Music music;
     Shader circle;
     float out_smear[CAPACITY];
+    float ALPHA;
 } Plug;
 
 Plug *plug;
-float ALPHA;
 
 bool draw_in_terminal = false;
 
@@ -117,6 +117,7 @@ void plug_init(const char *filepath) {
     /*memset(plug->smoothed, 0, sizeof(plug->smoothed));*/
 }
 float get_amp(float complex z, int vis_type) {
+    //applies log on demand to make the visualisation logarithmic 'on demand'
     if (vis_type == VISUALISATION_NORMAL) {
 	return cabsf(z);
     } else {
@@ -126,14 +127,8 @@ float get_amp(float complex z, int vis_type) {
     }
 }
 
-void plug_update() {
-    ALPHA = 0.2f;
-    plug->MAX_SAMPLE = 0.0f;
-    int w = GetRenderWidth();
-    int h = GetRenderHeight();
-    float step = 1.06;
-    size_t m = 0;
-    UpdateMusicStream(plug->music);
+void check_for_keys_and_files(){
+
     if (IsKeyPressed(KEY_SPACE)) {
 	if (IsMusicStreamPlaying(plug->music)) {
 	    PauseMusicStream(plug->music);
@@ -175,16 +170,29 @@ void plug_update() {
 	}
 	UnloadDroppedFiles(droppedFiles);
     }
-
-    memcpy(plug->fft_input, plug->global_samples,
-           sizeof(plug->fft_input)); // this is done so the values of plug->global_samples do not get overwritten by the
-                                     // callback(which runs in a different thread) before the fft is finished
+}
+void hann_windowing(){
 
     for (size_t i = 0; i < CAPACITY; i++) { // applying hann windowing to get rid of phantom frequencies
 	float t = (float)i / CAPACITY;
 	float hann = 0.5f - 0.5f * cosf(2 * PI * t);
 	plug->fft_input[i] = plug->fft_input[i] * hann;
     }
+}
+void plug_update() {
+    plug->ALPHA = 0.3f; 
+    plug->MAX_SAMPLE = 0.0f;
+    int w = GetRenderWidth();
+    int h = GetRenderHeight();
+    float step = 1.06;
+    size_t m = 0;
+    UpdateMusicStream(plug->music);
+    check_for_keys_and_files();
+
+    memcpy(plug->fft_input, plug->global_samples,
+           sizeof(plug->fft_input)); // this is done so the values of plug->global_samples do not get overwritten by the
+                                     // callback(which runs in a different thread) before the fft is finished
+    hann_windowing();
     fft(plug->fft_input, 1, plug->fft_global_samples, CAPACITY);
     //////////////////////////////////////////////////
     for (size_t i = 0; i < CAPACITY; i++) {
@@ -193,111 +201,116 @@ void plug_update() {
 	    plug->MAX_SAMPLE = t; // Finding max_sample so i can normalise values later
     }
     //////////////////////////////////////////////////
+    BeginDrawing();
     if (plug->MAX_SAMPLE == 0.0f) {
-	// printf("MAX_SAMPLE was %f skipping drawing\n",plug->MAX_SAMPLE);
+        //Something went wrong since max sample was 0, skipping drawing frame.
 	ClearBackground(CLITERAL(Color){0x18, 0x18, 0x18, 0xFF});
-	printf("\033[2J");
 	EndDrawing();
 	return;
     }
-    BeginDrawing();
     ClearBackground(CLITERAL(Color){0x18, 0x18, 0x18, 0xFF});
     /////////////////////////////////////////////////
-    for (float f = 1.0f; (size_t)f < CAPACITY / 2;
-         f = ceilf(
-             f * step)) { // here im applying ceiling since 1*1,06 is 1.06 and we are using it as index(int) so it gets
-	                  // truncated to 1 , making it so im drawing the same freq over and over till it gets to 2
-
+    for (float f = 1.0f; (size_t)f < CAPACITY / 2; f = ceilf( f * step)) { 
+        // here im applying ceiling since 1*1,06 is 1.06 and we are using it as index(int) so it gets
+        // truncated to 1 , making it so im drawing the same freq over and over till it gets to 2
+        // Finding how many values actually are there since im going for the 'f*=step' stride. also im only iterating
+        // CAPACITY/2 because output of fft mirrors after n/2 samples 
 	m += 1;
-    } // Finding how many values actually are there since im going for the 'f*=step' stride. also im only iterating
-      // CAPACITY/2 because output of fft mirrors after n/2 samples
-    float cell_width = (float)w / (m); // The second half holds no meaningful/new information.
-    /////////////////////////////////////////////////
+    } 
+    
+    float cell_width = (float)w / (m); 
+                                       
     int s = m;
     m = 0;
-
-    Texture2D texture = {rlGetTextureIdDefault(), 1, 1, 1, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8
-
-    };
-    for (float f = 1.0f; (size_t)f < CAPACITY / 2; f = ceilf(f * step)) {
-
+    Texture2D texture = {rlGetTextureIdDefault(), 1, 1, 1, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8}; //preparing blank 'custom' texture to be drawn laater
+    for (float f = 1.0f; (size_t)f < CAPACITY / 2; f = ceilf(f * step)) {                       // going until CAPACITY/2 because second half is a mirror of first half.
 	float f1 = ceilf(f * step);
-	float a = 0.0f;
+	float max = 0.0f;
 	/////////////////////////////////////////////////////////////////////
 	for (size_t q = (size_t)f; q < (CAPACITY / 2) && q < (size_t)f1; ++q) {
 
-	    if (get_amp(plug->fft_global_samples[q], plug->visulalisation_type) > a)
-		a = get_amp(plug->fft_global_samples[q],
-		            plug->visulalisation_type); // getting max of the skiped values (values between f and f1) so
-		                                        // they are not lost completely
-	    // a+= cabs(plug->fft_global_samples[q]);
+	    if (get_amp(plug->fft_global_samples[q], plug->visulalisation_type) > max)
+		max = get_amp(plug->fft_global_samples[q],
+		            plug->visulalisation_type); // getting max of the skiped values (values between f and f1) so they are not lost completely
+	    
 
 	} // This is done to turn it into a logarithmic visualisation(?), not sure why this works or how it works
-	// a=a/((size_t)f1 - (size_t)f +1);
 	////////////////////////////////////////////////////////////////////
-	float t = a / plug->MAX_SAMPLE;
+	float norm_value = max / plug->MAX_SAMPLE;
 
-	/* if (t/plug->smoothed[m]>1.6f || t/plug->smoothed[m]<0.4f ) ALPHA=0; */
+	/* if (t/plug->smoothed[m]>1.6f || t/plug->smoothed[m]<0.4f ) plug->alpha=0; */
 
 	//printf("%f %f\n", plug->smoothed[m], plug->out_smear[m]);
 	plug->smoothed[m] =
-	    plug->smoothed[m] * ALPHA +
-	    t * (1 - ALPHA); // this is to smooth it out , since fft is being calculated like 60 times a second, the
-	                     // output values go up and down around the center value and visually this renders
-	plug->out_smear[m] += (plug->smoothed[m] - plug->out_smear[m]) * 0.05f;
-	float barH = plug->smoothed[m] * h /
-	             2;     // as flickering which is visually unappealing. to go around this we only use 30% of the new
-	                    // value and 70% of the old value, so that the value we visualise is closer to old value.
-	if (barH > h / 2) { // this is called Exponential Moving Average(EMA)
+	    plug->smoothed[m] * (1-plug->ALPHA) + norm_value *  plug->ALPHA; // this is to smooth it out , since fft is being calculated like 60 times a second, the
+                                                                             // output values go up and down around the center value and visually this renders
+                                                                              // as flickering which is visually unappealing. to go around this we only use 30% of the new 
+                                                                              // value and 70% of the old value, so that the value we visualise is closer to old value.    
+                                                                              // this is called Exponential Moving Average(EMA)                                            
+
+	//plug->out_smear[m] += (plug->smoothed[m] - plug->out_smear[m]) * 0.05f;  //constant here changes how fast the smear 'catcches up to' to the circle
+          plug->out_smear[m] +=   plug->smoothed[m]*0.05f - plug->out_smear[m]*0.05f;
+        // 8 , 8-0=8, 7,7-8=-1+8=10                                                                               
+        float barH = plug->smoothed[m] * h / 2;  //getting bar height   
+	if (barH > h / 2) { 
 	    barH = h / 2;
 	}
 
-	float hue = (float)m / s;
+	float hue = (float)m / s; //m-th valu divided by the total number of values there are, to get the angle of the 'knob' in radian. this needs to be multiplied by 360*  to turn to degrees
 	float saturation = 0.75f;
 	float value = 1.0f;
 	Color color = ColorFromHSV(hue * 360, saturation, value);
-	// DrawRectangle(m * cell_width, h / 2 - (int)barH, ceilf(cell_width), barH, color);
-	float radius = 8 * cell_width * sqrtf(plug->smoothed[m]);
-	float thickness =
-	    (cell_width / 2) * sqrtf(plug->smoothed[m]); // doing sqrt to ease it , see resources. but basically sqr(x)
-	                                                 // grows faster than x for 0<x<1;
-
+	float radius = 8 * cell_width * sqrtf(plug->smoothed[m]);  // doing sqrt to ease it , see resources. but basically sqr(x)
+	float thickness =                                          // grows faster than x for 0<x<1;                             
+	    (cell_width / 2) * sqrtf(plug->smoothed[m]); 
+	                                                 
+        //Drawing the line/colon
 	Vector2 startPos = {.x = m * cell_width, .y = h / 2 - (int)barH};
 	Vector2 endPos = {.x = m * cell_width, .y = h / 2};
 	DrawLineEx(startPos, endPos, thickness, color);
-	Rectangle rec = {.x = startPos.x - radius, .y = startPos.y - radius, .width = 2 * radius, .height = 2 * radius
 
-	};
+	//Rectangle rec = {.x = startPos.x - radius, .y = startPos.y - radius, .width = 2 * radius, .height = 2 * radius };
 
-	Vector2 position = {
+        //position for the texture
+	Vector2 texture_position = {
 	    .x = startPos.x - radius,
 	    .y = startPos.y - radius,
 	};
 
-	float smear_offset = (plug->smoothed[m] - plug->out_smear[m]) * h / 2;
-	Vector2 pos = {.x = startPos.x , .y = startPos.y + smear_offset};
+        //drawing texture in shader mode
 	BeginShaderMode(plug->circle);
-	DrawTextureEx(texture, position, 0, 2 * radius, color);
+	DrawTextureEx(texture, texture_position, 0, 2 * radius, color);
 	EndShaderMode();
-        float rect_h=startPos.y-pos.y;
+
+        
+	float smear_offset = (plug->smoothed[m] - plug->out_smear[m]) * h / 2;
+	Vector2 smear_position = {.x = startPos.x , .y = startPos.y + smear_offset};
+        float smear_height=startPos.y-smear_position.y;
         Rectangle rect;
-        if (rect_h<0){
-            rect_h*=-1;
+        //handling when smear height is negative , which means that the rect needs to be drawn on the other side of the y axis of the circle
+        if (smear_height<0){
+            smear_height*=-1;
             rect.x = startPos.x - cell_width/2;
             rect.y= startPos.y;//startPos.y,
             rect.width=cell_width;
-            rect.height=rect_h;
+            rect.height=smear_height;
 
         }else{
             rect.x = startPos.x - cell_width/2;
-            rect.y= pos.y;//startPos.y,
+            rect.y= smear_position.y;//startPos.y,
             rect.width=cell_width;
-            rect.height=rect_h;
+            rect.height=smear_height;
 
         }
-        DrawLineV(pos, (Vector2){.x=startPos.x,.y=startPos.y}, RED);
+        char p[4]={0};
+        snprintf(p,sizeof(p)," %zu ",m);
+        if (m<17){
+            printf("m:%zu circle pos: %f %f rect end pos: rect %f %f\n",m,smear_position.x,smear_position.y,rect.x+rect.height, rect.y+rect.height);
+
+        } 
         DrawRectangleRec(rect, color);
-	//DrawCircleV(pos, cell_width, RED);
+        DrawText(p,  endPos.x, endPos.y, 1, WHITE);
+	DrawCircleV(smear_position, cell_width, RED);
 	if (draw_in_terminal)
 	    draw_bar_in_terminal(m, plug->smoothed[m]);
 	m++;
